8a9f4351a4bba617c0af2e69f932f06a16987716
Abdullah Ibrahim Dar <139804220+Abdullah374@users.noreply.github.com>
2024-10-06 12:49:38 +0530

    Add files via upload

---
+++ missingnum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            key = i
+            if key in nums:
+                continue
+            else:
+                break
+        return key
