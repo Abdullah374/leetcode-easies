ef7b6cb8a9cc76d3892f3fbe36c92b87cb37521a
Abdullah Ibrahim Dar <139804220+Abdullah374@users.noreply.github.com>
2024-11-05 21:54:22 +0530

    Add files via upload

---
+++ 2022.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m*n != len(original):
+            return []
+        
+        result = []
+
+        for i in range(m):
+            result.append(original[i*n:(i+1)*n])
+        return result
